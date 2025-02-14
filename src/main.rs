435ec3097792a05410d91901b80e6fde14e66498
𝘚.𝘊. 𝘓𝘰𝘳𝘦𝘯𝘵𝘻 <113468900+SCLorentz@users.noreply.github.com>
2025-02-14 16:57:33 +0000

    String

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,44 @@
 #![no_main]
 
 use core::arch::asm;
+use exit_no_std::exit;
 
+mod string;
+use string::*;
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
+    exit(1)
 }
 
 #[no_mangle]
-pub extern "C" fn _start() {
-    // Código para imprimir a mensagem
-    unsafe {
-        let msg = b"Hello, World!";
+pub extern "C" fn _start()
+{
+    unsafe
+    {
+        let msg: String<12> = String::new(b"Hello, World");
         let size = msg.len();
-        let fd = 1; // stdout
+        let fd = 1;            // stdout
 
         let syscall_ret: u64;
         asm!(
-            "mov rax, 1",      // sys_write
-            "mov rdi, {}",     // file descriptor
-            "mov rsi, {}",     // endereço da mensagem
-            "mov rdx, {}",     // tamanho da mensagem
+            "mov rax, 1",           // sys_write
+            "mov rdi, {fd:r}",      // file descriptor
+            "mov rsi, {msg}",       // endereço da mensagem
+            "mov rdx, {size}",      // tamanho da mensagem
             "syscall",
-            in(reg) fd,
-            in(reg) msg.as_ptr(),
-            in(reg) size,
+            fd = in(reg) fd,
+            msg = in(reg) msg.as_bytes().as_ptr(),
+            size = in(reg) size,
             out("rax") syscall_ret,
             options(nostack, preserves_flags)
         );
 
         if syscall_ret != size as u64 {
-            loop {}
+            exit(1)
         }
 
-        loop {}
+        exit(0)
     }
 }
---
+++ src/string.rs
@@ -0,0 +1,23 @@
+pub struct String<'a, const N: usize>
+{
+    pub(crate) context: &'a [u8; N]
+}
+
+impl<'a, const N: usize> String<'a, N>
+{
+    pub fn new(context: &'a [u8; N]) -> String<'a, N>
+    {
+        String { context }
+    }
+
+    pub fn len(&self) -> usize
+    {
+        self.as_bytes().len()
+    }
+
+    #[allow(dead_code)]
+    pub fn as_bytes(&self) -> &'a [u8]
+    {
+        self.context
+    }
+}
