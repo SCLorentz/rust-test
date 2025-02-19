feec23cad07ed5b744943ad225d670b2b02612d2
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2025-02-19 22:50:22 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,29 @@ pub extern "C" fn _start() -> !
 *          Thanks to a woman,                                   *
 *          \x1b[4mKathleen Booth\x1b[0m, this code works!                     *
 *                                                               *
-=================================================================\n";
+=================================================================
+
+\"Like a girl\" - Como uma garota
+---
+A frase como uma garota, as vezes, especialmente no cotidiano de
+muitas mulheres, pode ser algo comum de se ouvir. Mas o que isso
+significa?
+
+Bem, para muitas crian\xC3\xA7as, isso parecia algo dito com inten\xC3\xA7\xC3\xB5es
+ruins. Para muitas mulheres j\xC3\xA1 adultas isso representava um
+trauma, pois \xC3\xA9 algo muitas vezes usado como justificativa sobre
+o que uma mulher pode e n\xC3\xA3o pode fazer, como se ela n\xC3\xA3o tivesse
+capacidade de fazer tal coisa.
+
+Algo relatado no video \xC3\xA9 como durante o inicio da puberdade a
+confian\xC3\xA7a dessas garotas diminui, e especialmente nessa fase, os
+comentarios realmente podem machucar. E as intrevistadas
+afirmaram que fariam as coisas de forma diferente caso ouvissem
+isso hoje. Correriam do seu jeito e continuariam com o que
+estavam fazendo. Porque elas correm como garotas, escovam os
+dentes como garotas e acordam como garotas, porque elas s\xC3\xA3o
+garotas.\n";
+
     write(msg);
 
     // blinking_text(b"\x1b[31mHello, World!\x1b[0m");
